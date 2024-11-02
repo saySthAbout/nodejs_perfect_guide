1095ba862ded9e4d9a5535f650533e054b2c4d91
JinAh Kwak <saysthabout@gmail.com>
2024-11-02 21:58:27 +0900

    14. 화살표 함수의 이해

---
+++ play.js
@@ -0,0 +1,19 @@
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+const summrizeUser = (userName, userAge, userHasHobby) => {
+    return ('Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby);
+};
+
+//const addOne = a => a + 1;
+// const addOne = (a) => a + 1;
+const addRandom = () => 1 + 2;
+
+// console.log(add(1, 2));
+console.log(addOne(1));
+console.log(addRandom());
+
+console.log(summrizeUser(name, age, hasHobbies));
