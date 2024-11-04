1c1564d2b5fafa413afd0459d22f69a9b14b15cf
JinAh Kwak <58837220+saySthAbout@users.noreply.github.com>
2024-11-04 11:27:49 +0900

    15. 개체, 속성 및 메서드 작업

--- play.js
+++ play.js
@@ -1,19 +1,9 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
-
-age = 30;
-
-const summrizeUser = (userName, userAge, userHasHobby) => {
-    return ('Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby);
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
 };
 
-//const addOne = a => a + 1;
-// const addOne = (a) => a + 1;
-const addRandom = () => 1 + 2;
-
-// console.log(add(1, 2));
-console.log(addOne(1));
-console.log(addRandom());
-
-console.log(summrizeUser(name, age, hasHobbies));
+person.greet();
